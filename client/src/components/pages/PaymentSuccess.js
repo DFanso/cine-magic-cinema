35f804f12c3e2e3423c2de848005c2da81b8a077
Vidwa De Seram <vidwadeseram2002@gmail.com>
2024-01-04 18:34:32 +0530

    payment-loading -done

--- client/src/components/pages/PaymentSuccess.js
+++ client/src/components/pages/PaymentSuccess.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import '../css/PaymentSucess.css'; // Import your CSS file for styling
+import React, { useEffect } from "react";
+import "../css/PaymentSucess.css"; // Import your CSS file for styling
+
+import { TailSpin } from "react-loader-spinner";
+import { useLoading } from "../LoadingContext.js";
 
 const PaymentSuccess = () => {
-    return (
-        <div className="payment-success-container">
-            <h1 className="payment-success-text">Payment Successful!</h1>
-            <p className="payment-success-message">
-                Thank you for your payment. Your transaction was successful.
-            </p>
+  const { loading, setLoading } = useLoading();
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 15000); // Set the timeout for 15 seconds
+
+    return () => clearTimeout(timer); // Cleanup the timer
+  }, [setLoading]);
+
+  return (
+    <div className={`login-wrapper ${loading ? "blurred" : ""}`}>
+      {loading && (
+        <div className="loader-container">
+          <TailSpin color="#00BFFF" height={100} width={100} />
         </div>
-    );
+      )}
+      <div className="payment-success-container">
+        <h1 className="payment-success-text">Payment Successful!</h1>
+        <p className="payment-success-message">
+          Thank you for your payment. Your transaction was successful.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default PaymentSuccess;
