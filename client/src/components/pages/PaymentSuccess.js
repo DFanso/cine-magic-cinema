8fb87f9f8545d960701ae1a4c9ca822a054355ec
Asanda65 <93676091+Asanda65@users.noreply.github.com>
2024-01-04 17:48:47 +0530

    Update scroll issue, payment cancel and success page

--- client/src/components/pages/PaymentSuccess.js
+++ client/src/components/pages/PaymentSuccess.js
@@ -7,6 +7,7 @@ const PaymentSuccess = () => {
             <h1 className="payment-success-text">Payment Successful!</h1>
             <p className="payment-success-message">
                 Thank you for your payment. Your transaction was successful.
+                <div className='success-icon'><img className='yes-img' src="/images/yes.png" alt="Success Icon"></img></div>
             </p>
         </div>
     );
