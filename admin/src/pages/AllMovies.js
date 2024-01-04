ebfbecd3d5470f2b4c88a1acabe26638d0205049
Vidwa De Seram <vidwadeseram2002@gmail.com>
2024-01-04 21:54:27 +0530

    admin movie fetch and auth-done

--- admin/src/pages/AllMovies.js
+++ admin/src/pages/AllMovies.js
@@ -1,74 +1,79 @@
-import React from 'react';
-import '../css/AllMovies.css'; // Ensure you have a MovieCard.css file in the same directory
-import { Link } from 'react-router-dom';
+import "../css/AllMovies.css"; // Ensure you have a MovieCard.css file in the same directory
+import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
-const movies = [
-    // JSON-like array of movie objects
-    {
-        "id": 1,
-        "title": "Wonka",
-        "coverImage": "/images/wonka.jpeg"
-    },
-    {
-        "id": 2,
-        "title": "Marvels",
-        "coverImage": "/images/marvels1.jpg"
-    },
-    {
-        "id": 3,
-        "title": "Migration",
-        "coverImage": "/images/migration.jpg"
-    },
-    {
-        "id": 4,
-        "title": "Aquaman",
-        "coverImage": "/images/aquaman-cover-2.jpg"
-    }
-    // ... add more movie objects as needed
-];
+import axios from "axios";
 
-
-const MovieCard = ({ movie }) => {
-    return (
-        <div className="movie-card">
-            <img src={movie.coverImage} alt={movie.title} className="movie-cover" />
-            <div className="movie-info">
-                <h3>{movie.title}</h3>
-                <div className="movie-actions">
-                    <Link to="/UpdateMovie" className="btn-link">
-                        <button className="new-btn">Update</button>
-                    </Link>
-                    <Link to="/showtime-view" className="btn-link">
-                        <button className="show-btn">Showtime</button>
-                    </Link>
-                    <Link to="/DeleteMovie" className="btn-link">
-                        <button className="new-btn-del">Delete</button>
-                    </Link>
-                </div>
-            </div>
+const MovieCard = ({ movie, isComingSoon }) => {
+  return (
+    <div className="movie-card">
+      <img src={movie.coverImage} alt={movie.title} className="movie-cover" />
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        {isComingSoon && <p className="coming-soon">Coming Soon</p>}
+        <div className="movie-actions">
+          <Link to="/UpdateMovie" className="btn-link">
+            <button className="new-btn">Update</button>
+          </Link>
+          <Link to="/showtime-view" className="btn-link">
+            <button className="show-btn">Showtime</button>
+          </Link>
+          <Link to="/DeleteMovie" className="btn-link">
+            <button className="new-btn-del">Delete</button>
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-
 const MovieGrid = () => {
-    return (
-        <div>
-            <div className="add-movie-button-container">
-                <Link to="/AddMovie" className="btn-link">
-                    <Link to="/movies" className="btn-link">
-                        <button className="btn add-btn">Add Movie</button>
-                    </Link>
+  const [currentMovies, setCurrentMovies] = useState([]);
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
 
-                </Link>
-            </div>
-            <div className="movie-grid">
-                {movies.map((movie, index) => (
-                    <MovieCard key={index} movie={movie} />
-                ))}
-            </div>
-        </div>
-    );
+  const fetchMovies = async (nowShowing) => {
+    try {
+      const adminToken = localStorage.getItem("admin-token");
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_PATH}/movies`,
+        {
+          params: { nowShowing },
+          headers: { Authorization: `Bearer ${adminToken}` },
+        }
+      );
+      if (nowShowing) {
+        setCurrentMovies(response.data);
+      } else {
+        setUpcomingMovies(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(true); // Fetch current movies
+    fetchMovies(false); // Fetch upcoming movies
+  }, []);
+  return (
+    <div>
+      <div className="add-movie-button-container">
+        <Link to="/AddMovie" className="btn-link">
+          <Link to="/movies" className="btn-link">
+            <button className="btn add-btn">Add Movie</button>
+          </Link>
+        </Link>
+      </div>
+      <div className="movie-grid">
+        {currentMovies.map((movie, index) => (
+          <MovieCard key={index} movie={movie} isComingSoon={false} />
+        ))}
+        {upcomingMovies.map((movie, index) => (
+          <MovieCard key={index} movie={movie} isComingSoon={true} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MovieGrid;
--- admin/src/pages/Login.js
+++ admin/src/pages/Login.js
@@ -1,43 +1,93 @@
-// Login.js
-import React, { useState } from 'react';
-import '../css/Login.css'; // Import the CSS file
-import { Link } from 'react-router-dom';
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Swal from "sweetalert2";
 
-const Login = ({ onLogin }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+import { UserContext } from "./auth/UserContext";
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Implement your login logic here
-        onLogin();
-    };
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const { updateUserData } = useContext(UserContext); // Use the context to set user data
 
-    return (
-        <div className="login-container">
-            <form onSubmit={handleSubmit} className="login-form">
-                <h1>Login</h1>
-                <div className="form-group">
-                    <label className='label-name'>Email:</label><br />
-                    <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-                </div>
-                <div className="form-group">
-                    <label className='label-name'>Password:</label><br />
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-                </div>
-                <div className='log-reg-con'>
-                    <div className='login-btn-div'>
-                        <button type="submit" className="login-button">Login</button>
-                    </div>
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // Authenticate user
+      const loginResponse = await axios.post(
+        `${process.env.REACT_APP_API_PATH}/auth/signin`,
+        { email: username, password }
+      );
 
+      const token = loginResponse.data.accessToken;
+      // Fetch user profile
+      const profileResponse = await axios.get(
+        `${process.env.REACT_APP_API_PATH}/users/profile`,
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      console.log(profileResponse);
+      const userData = profileResponse.data; // User data from profile response
 
-                    <Link to="/register-page" className="btn-link">
-                        <p className="reg-link">Register</p>
-                    </Link>
-                </div>
-            </form>
+      if (userData && userData.type === "ADMIN") {
+        localStorage.setItem("admin-token", token); // Store the token
+        updateUserData(userData); // Update user data in context
+        console.log(userData);
+        navigate("/movies-view"); // Navigate to the admin dashboard
+      } else {
+        Swal.fire({
+          title: "Access Denied",
+          text: "You are not authorized to access the admin panel.",
+          icon: "error",
+          confirmButtonText: "OK",
+        });
+      }
+    } catch (error) {
+      console.error("Login Error:", error);
+      Swal.fire({
+        title: "Login Failed",
+        text: "Invalid username or password.",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+    }
+  };
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h1>Login</h1>
+        <div className="form-group">
+          <label className="label-name">Email:</label>
+          <br />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
-    );
+        <div className="form-group">
+          <label className="label-name">Password:</label>
+          <br />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="log-reg-con">
+          <div className="login-btn-div">
+            <button type="submit" className="login-button">
+              Login
+            </button>
+          </div>
+
+          <Link to="/register-page" className="btn-link">
+            <p className="reg-link">Register</p>
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
